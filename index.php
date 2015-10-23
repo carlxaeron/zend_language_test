053715bd4ff9136fb8b344d272335e196052d4e4
carl louis  manuel <carlxaeron09@gmail.com>
2015-10-23 10:43:02 +0000

    =updates

--- index.php
+++ index.php
@@ -6,10 +6,7 @@ if (file_exists('vendor/autoload.php')) {
 $translator = new Zend\I18n\Translator\Translator;
 $translator->setLocale('en_US');
 
-//\Zend\Debug\Debug::dump($translator->getLocale());
-
 $translator->addTranslationFilePattern('gettext', __DIR__ . '/texts', '%s.mo');
-//\Zend\Debug\Debug::dump(get_class_methods($translator));die();
 ?>
 
 <?= $translator->translate('hahahaha test'); ?>
